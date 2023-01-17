86cd9ab9861631527a5bf795752e7011b60d6dd7
LeonardoLeone <84713044+LeonardoLeone03@users.noreply.github.com>
2023-01-17 16:28:20 +0100

    Create App.java

---
+++ popup/src/App.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+import javax.swing.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        for(;;){
+        JFrame f = new JFrame();
+        JButton b = new JButton("SONO CRISTIANO FROCIO");
+        
+        f.add(b);
+        f.setSize(500,250);
+        f.setLocation(100,200);
+        f.setLayout(null);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+}
+}
